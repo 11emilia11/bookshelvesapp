93aec434a826a5d0979105cd4dea9fbb33c21c46
Emilia Galdino <emilia.galdino@ufrpe.br>
2021-07-09 18:39:29 -0300

    book detail layout ok

--- lib/book/book_detail_page.dart
+++ lib/book/book_detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:bookshelvesapp/core/app_colors.dart';
+import 'package:bookshelvesapp/core/app_text_styles.dart';
 
 class BookDetailPage extends StatefulWidget {
   @override
@@ -17,7 +19,10 @@ class _BookDetailPageState extends State<BookDetailPage> {
             
             children: [
               Container(
-                decoration: BoxDecoration(color: Colors.blueGrey[300], borderRadius:  BorderRadius.circular(10.0) ),
+                decoration: BoxDecoration(
+                  border: Border.fromBorderSide(BorderSide(color: AppColors.border)),color: AppColors.white,
+                  borderRadius: BorderRadius.circular(10)
+                  ),
                 
                 margin: const EdgeInsets.all(16),
                 padding: const EdgeInsets.all(8.0),
@@ -26,16 +31,17 @@ class _BookDetailPageState extends State<BookDetailPage> {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Text('The Lord of the Flies', style: TextStyle(fontWeight: FontWeight.w800, fontSize: 25, color: Colors.white), ),
-                      Text('William Golding', style: TextStyle(fontWeight: FontWeight.w200, fontSize: 20, color: Colors.white), ),
+                      Text('The Lord of the Flies', style: AppTextStyles.heading ),
+                      Text('William Golding', style: AppTextStyles.heading15 ),
                       Icon(Icons.book_rounded, size: 128,
-                       color: Colors.white),
+                       color: AppColors.lightGrey),
                       Row(
+
                         mainAxisAlignment: MainAxisAlignment.center,
                         
                         children: [
                           Icon(Icons.star, color: Colors.yellow,),
-                          Text('3', style: TextStyle(fontWeight: FontWeight.w400))
+                          Text('3', style: AppTextStyles.body)
 
                       ],)],
                   ),),
@@ -68,12 +74,14 @@ class _BookDetailPageState extends State<BookDetailPage> {
               Container(
                 padding: EdgeInsets.all(16),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
                       margin: EdgeInsets.all(8),
+                      padding: const EdgeInsets.only(left: 128, right: 128, top: 4, bottom: 4),
                       decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0),
-                     color:Colors.blueGrey[300] ),
+                      
+                     color: AppColors.darkGreen ),
                       child: TextButton(
                         style: TextButton.styleFrom(
                           textStyle: const TextStyle(fontSize: 16)),
@@ -83,14 +91,15 @@ class _BookDetailPageState extends State<BookDetailPage> {
                     
                     Container(
                      margin: EdgeInsets.all(8),
+                     padding: const EdgeInsets.only(left: 128, right: 128, top: 4, bottom: 4),
                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0),
-                     border: Border.all(width: 1, color: Colors.blueGrey[300]),
+                     border: Border.all(width: 1, color:  Color(0xFFE1E1E6)),
                      color:Colors.white),
                       child: TextButton(
                         style: TextButton.styleFrom(
                           textStyle: const TextStyle(fontSize: 16)),
                         onPressed: () {},
-                        child: const Text('Buscar Livros', style: TextStyle(color: Colors.blueGrey)),),
+                        child: const Text('Buscar', style: TextStyle(color:  Color(0xFFA6A1B2))),),
                     ),
                     
                   ],
